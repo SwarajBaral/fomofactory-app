562538e86826fc7dc83b009ae1c83c189e469cf1
Swaraj Baral <swaraj.baral@raft.ai>
2024-08-03 11:16:08 +0530

    Removed node-cron. Edited vercel.json

--- src/app/api/poll/route.ts
+++ src/app/api/poll/route.ts
@@ -32,45 +32,48 @@ const fetchAssetData = async () => {
 
 export async function GET() {
   try {
-    cron.schedule("*/15 * * * * *", async () => {
-      const data: Array<{
-        name: string;
-        symbol: string;
-        rate: number;
-        code: string;
-        volume: number;
-        delta: Record<string, number>;
-        cap: number;
-      }> = await fetchAssetData();
-      if (data) {
-        const dbData = [];
-        for (const asset of data) {
-          const assetData = {
-            name: asset.name,
-            symbol: asset.symbol ?? "",
-            price: asset.rate,
-            timestamp: new Date(),
-            code: asset.code,
-            meta: {
-              cap: asset.cap,
-              volume: asset.volume,
-              delta: asset.delta,
-            },
-          };
-          dbData.push(assetData);
-        }
-        await db.asset.createMany({ data: dbData });
-      } else {
-        return NextResponse.json(
-          { message: "Failed to fetch data" },
-          { status: 500 },
-        );
+    const data: Array<{
+      name: string;
+      symbol: string;
+      rate: number;
+      code: string;
+      volume: number;
+      delta: Record<string, number>;
+      cap: number;
+    }> = await fetchAssetData();
+    if (data) {
+      const dbData = [];
+      for (const asset of data) {
+        const assetData = {
+          name: asset.name,
+          symbol: asset.symbol ?? "",
+          price: asset.rate,
+          timestamp: new Date(),
+          code: asset.code,
+          meta: {
+            cap: asset.cap,
+            volume: asset.volume,
+            delta: asset.delta,
+          },
+        };
+        dbData.push(assetData);
       }
-    });
-    return NextResponse.json({ message: "Asset data saved" }, { status: 200 });
+      await db.asset.createMany({ data: dbData });
+      return NextResponse.json(
+        { message: "Asset data saved" },
+        { status: 200 },
+      );
+    } else {
+      console.log(`Empty data received: ${data}`);
+      return NextResponse.json(
+        { message: "Failed to fetch data" },
+        { status: 500 },
+      );
+    }
   } catch (e) {
+    console.error(`Something went wrong: ${e}`);
     return NextResponse.json(
-      { message: "Something went wrong while croning poll" },
+      { message: "Something went wrong while polling data" },
       { status: 500 },
     );
   }
